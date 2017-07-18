01eef5132ee010aabae0ae8209354115b14eed32
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2017-07-18 23:49:41 +0530

    directory structure change

---
+++ Rita/action_train.py
@@ -0,0 +1,20 @@
+import json
+#importing functions for training and predicting the model
+from secret_sauce.models import training, predict_action
+#importing torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch
+
+training_data = []
+foss = open('action_dataset.json', 'r')
+for line in foss:
+    #fetching training data
+    training_data.append(json.loads(line))
+
+training_data = training_data[0]
+training(10000, training_data) #training the model
+#sample
+predict_action("good bye")
+
+
