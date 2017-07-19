081f258ead41363db0897c8e4257a17afcb40def
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2017-07-19 19:04:34 +0530

    program for training the action model

---
+++ action_train.py
@@ -0,0 +1,20 @@
+import json
+from secret_sauce.models import training, predict_action
+#importing functions for training and predicting the model
+import torch.nn as nn
+#importing torch
+from torch.autograd import Variable
+import torch
+
+training_data = []
+foss = open('action_dataset.json', 'r')
+for line in foss:
+    #fetching training data
+    training_data.append(json.loads(line))
+
+training_data = training_data[0]
+training(10000, training_data) #training the model
+#sample
+predict_action("good bye")
+
+
