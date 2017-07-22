7457da13a37422c3af16361fb35330373c1e0ee4
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2017-07-23 02:23:25 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
-
-class InputForm(FlaskForm):
-    input_data = StringField('openid', validators=[DataRequired()])
