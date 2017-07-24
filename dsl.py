b4dc7cafa53303044e3f0cd52a1af7a4f029b046
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2017-07-24 12:19:50 +0530

    added url & postback action

--- dsl.py
+++ dsl.py
@@ -1,7 +1,8 @@
 import random
 class dsl(object):
     def __init__(self,intent):
-        self.rep = "error"
+        self.rep = {}
+
         self.website(intent)
         self.goodbye(intent)
         self.contact(intent)
@@ -11,16 +12,20 @@ class dsl(object):
         return self.rep
     def website(self,intent):
         if intent == "website":
-            self.rep = random.choice(["here is the RIT website:(www.rit.ac.in)","check this out:(www.rit.ac.in)","click here:(www.rit.ac.in)"])
+            self.rep["text"] = random.choice(["here is the RIT website:(www.rit.ac.in)","check this out:(www.rit.ac.in)","click here:(www.rit.ac.in)"])
+            self.rep["title"] = "college website"
+            self.rep["url"] = "www.rit.ac.in"
     def goodbye(self,intent):
         if intent == "goodbye":
-            self.rep = random.choice(["bye see you","have a nice day","good bye","take care","see you soon"])
+            self.rep["text"] = random.choice(["bye see you","have a nice day","good bye","take care","see you soon"])
     def contact(self,intent):
         if intent == "contact":
-            self.rep = random.choice(["here is some information:(8281427437)","here you go:(8281427437)","contact:(8281427437)"])
+            self.rep["text"] = random.choice(["here is some information:(8281427437)","here you go:(8281427437)","contact:(8281427437)"])
+            self.rep["post_back"] = True
+            self.rep["post_content"] = "8281427437"
     def location(self,intent):
         if intent == "location":
-            self.rep = random.choice(["here is the address:(address)","i will navigate you:(address)","RIT is here:(address)","Look at this:(address)"])
+            self.rep["text"] = random.choice(["here is the address:(address)","i will navigate you:(address)","RIT is here:(address)","Look at this:(address)"])
     def greetings(self,intent):
         if intent == "greeting":
-            self.rep = random.choice(["hello there","Hi","hi there","its good to see you","hello"])
+            self.rep["text"] = random.choice(["hello there","Hi","hi there","its good to see you","hello"])
