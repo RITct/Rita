54041648c7ae5240093ece9955679d6d14bd62ec
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2017-07-26 23:25:59 +0530

    code for training seqtoseq model

---
+++ seqtoseq_train.py
@@ -0,0 +1,17 @@
+import json
+from secret_sauce.seqtoseq_model import seqtoseq_train, reply_predict
+import torch.nn as nn
+#importing torch
+from torch.autograd import Variable
+import torch
+training_data = []
+
+with open('chitchat_dataset.json') as data_file:
+    data = json.load(data_file)
+
+for line in data:
+    #fetching training data
+    training_data.append((line["question"],line["answer"]))
+seqtoseq_train(10000,training_data)
+print(reply_predict("any problem"))
+#print(training_data[0])
