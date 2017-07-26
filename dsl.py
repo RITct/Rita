6c0678ed917c7d2526a9d003b904026462db2134
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2017-07-26 23:30:57 +0530

    dsl changed

--- dsl.py
+++ dsl.py
@@ -4,10 +4,10 @@ class dsl(object):
         self.rep = {}
 
         self.website(intent)
-        self.goodbye(intent)
+
         self.contact(intent)
         self.location(intent)
-        self.greetings(intent)
+
     def generate(self):
         return self.rep
     def website(self,intent):
@@ -15,9 +15,7 @@ class dsl(object):
             self.rep["text"] = random.choice(["here is the RIT website:(www.rit.ac.in)","check this out:(www.rit.ac.in)","click here:(www.rit.ac.in)"])
             self.rep["title"] = "college website"
             self.rep["url"] = "www.rit.ac.in"
-    def goodbye(self,intent):
-        if intent == "goodbye":
-            self.rep["text"] = random.choice(["bye see you","have a nice day","good bye","take care","see you soon"])
+
     def contact(self,intent):
         if intent == "contact":
             self.rep["text"] = random.choice(["here is some information:(8281427437)","here you go:(8281427437)","contact:(8281427437)"])
@@ -26,6 +24,4 @@ class dsl(object):
     def location(self,intent):
         if intent == "location":
             self.rep["text"] = random.choice(["here is the address:(address)","i will navigate you:(address)","RIT is here:(address)","Look at this:(address)"])
-    def greetings(self,intent):
-        if intent == "greeting":
-            self.rep["text"] = random.choice(["hello there","Hi","hi there","its good to see you","hello"])
+
